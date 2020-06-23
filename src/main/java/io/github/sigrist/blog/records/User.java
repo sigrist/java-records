1b8d71fc6bf7f4a99ab7c525abb515fc7ce657c1
Paulo Sigrist <paulo.sigrist@gmail.com>
2020-06-22 21:55:35 -0300

    First test with Java Records

---
+++ src/main/java/io/github/sigrist/blog/records/User.java
@@ -0,0 +1,13 @@
+package io.github.sigrist.blog.records;
+
+/**
+ * Record representing an User data.
+ *
+ * @since 1.0.0
+ * 
+ * @param id    The user ID
+ * @param login The user login
+ */
+public record User(final Long id, final String login) {
+
+}
---
+++ src/test/java/io/github/sigrist/blog/records/UserTest.java
@@ -0,0 +1,50 @@
+package io.github.sigrist.blog.records;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * UnitTests for record User.
+ *
+ */
+public class UserTest {
+
+	/**
+	 * Test create an User and assert if the fields are ok.
+	 */
+	@Test
+	public void testCreateUser() {
+		final User user = new User(1L, "sigrist");
+
+		assertEquals(1L, user.id());
+		assertEquals("sigrist", user.login());
+	}
+
+	/**
+	 * Test if two different instances created with same parameters are equals and
+	 * have the same hash code.
+	 */
+	@Test
+	public void testEqualsAndHashCode() {
+
+		final User user1 = new User(1L, "sigrist");
+		final User user2 = new User(1L, "sigrist");
+
+		assertEquals(user1, user2);
+		assertEquals(user1.hashCode(), user2.hashCode());
+
+	}
+
+	/**
+	 * Test the generated toString method.
+	 */
+	@Test
+	public void testToString() {
+		final User user = new User(1L, "sigrist");
+
+		assertEquals("User[id=1, login=sigrist]", user.toString());
+	}
+
+}
